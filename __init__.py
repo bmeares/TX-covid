1f9f6cc0285768fcd23a56d66bf2281ffbfafdd8
bmeares <bennett.meares@gmail.com>
2022-01-25 07:26:07 -0500

    Updated parser to match the new 2022 XLSX format

--- __init__.py
+++ __init__.py
@@ -7,12 +7,12 @@ Fetch county-level COVID-19 data for the state of Texas.
 """
 
 from __future__ import annotations
-from meerschaum.utils.typing import Optional, Dict, Any
+from meerschaum.utils.typing import Optional, Dict, Any, List
 from meerschaum.config._paths import PLUGINS_TEMP_RESOURCES_PATH
 import datetime
 import pathlib
 
-__version__ = '0.0.6'
+__version__ = '0.1.0'
 TMP_PATH = PLUGINS_TEMP_RESOURCES_PATH / 'TX-covid_data'
 XLSX_URL = "https://www.dshs.texas.gov/coronavirus/TexasCOVID19DailyCountyCaseCountData.xlsx"
 XLSX_PATH = TMP_PATH / 'Texas COVID-19 Case Count Data by County.xlsx'
@@ -74,24 +74,29 @@ def fetch(
     fips = pipe.parameters['TX-covid']['fips']
     fips_where = "'" + "', '".join(fips) + "'"
     counties_df = pd.read_csv(COUNTIES_PATH, dtype={'fips': str, 'counties': str, 'state': str})
-
     dtypes = {
         'date': 'datetime64[ms]',
         'county': str,
         'fips': str,
         'cases': int,
     }
-
-    wget(XLSX_URL, XLSX_PATH, debug=debug)
-    df = pd.read_excel(XLSX_PATH, skiprows=[0], header=1, nrows=254)
-    data = {'date': [], 'county': [], 'cases': [],}
-    counties = list(df['County Name'])
-
     begin = begin if begin is not None else (pipe.get_sync_time(debug=debug) if end is None else None)
+    sheets_to_read = None if begin is None else [f'Cases by County {year}' for year in get_years(begin, end)]
+    wget(XLSX_URL, XLSX_PATH, debug=debug)
+    sheets_dfs = pd.read_excel(XLSX_PATH, sheet_name=None, skiprows=[0], header=1, nrows=254)
+    df = pd.concat([df for k, df in sheets_dfs.items()], join='outer', axis=1)
+    data = {'date': [], 'county': [], 'cases': [],}
+    counties = list(df.iloc[:,0])
+
     if end is not None and begin is not None and end < begin:
         begin = end - datetime.timedelta(days=1)
     for col in df.columns[1:]:
-        date = parser.parse(col[len('Cases '):])
+        try:
+            date = parser.parse(str(col))
+        except parser._parser.ParserError:
+            continue
+        except TypeError:
+            print(f'{col=}')
         if begin is not None and date < begin:
             continue
         if end is not None and date > end:
@@ -129,3 +134,14 @@ def fetch(
         print(joined_df)
     return joined_df
 
+
+def get_years(
+        begin: datetime.datetime,
+        end: Optional[datetime.datetime] = None,
+    ) -> List[int]:
+    """
+    From the beginning datetime, get the years (as ints) that we are going to read.
+    """
+    import datetime
+    max_year = datetime.datetime.utcnow().year if end is None else end.year
+    return list(range(begin.year, max_year + 1))
